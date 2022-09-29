92acb69bac1a9f11c9210aa171e7f8bb66899d78
Nilufa Khan <khannilufa5@gmail.com>
2022-09-29 16:00:10 +0530

    initial commit

--- p4-weighted-average/p4-submission.js
+++ p4-weighted-average/p4-submission.js
@@ -1,5 +1,7 @@
 // define function calculateWeightedAverage here to calcuate weighted average
-
+function calculateWeightedAverage(num1,num2,num3,num4,num5){
+    return (num1+num2+num3+num4+num5)/5;
+}
 
 //calling functions
 console.log(`The weighted average score is : ${calculateWeightedAverage(97, 82, 60, 75, 80)}`);
